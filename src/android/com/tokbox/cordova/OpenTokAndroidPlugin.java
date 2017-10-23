65feea5ade1abe41a02dfee5d7e67529c92a64c7
albertoVieiraNeto <alberto.vieira@procfit.com.br>
2017-10-23 16:39:25 -0200

    teste

--- src/android/com/tokbox/cordova/OpenTokAndroidPlugin.java
+++ src/android/com/tokbox/cordova/OpenTokAndroidPlugin.java
@@ -198,8 +198,16 @@ public class OpenTokAndroidPlugin extends CordovaPlugin implements
         this.mView = mPublisher.getView();
         frame.addView( this.mView ); 
 
-		mSession.publish(mPublisher);	
-	
+		PluginResult result = null;
+		
+		try{
+			mSession.publish(mPublisher);			
+			result = new PluginResult(PluginResult.Status.OK, "");
+		}catch(Exception e){
+			result = new PluginResult(PluginResult.Status.Error, "erro ao publicar stream");
+		}
+
+        this.callbackContext.sendPluginResult(result);
 
       }
 	  
